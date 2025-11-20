c8b035b4cba3c2097d27c7dcb161fe31116fe4ce
Varvara Kornilova <vrvrmoren@yandex.ru>
2025-11-20 15:27:46 +0300

    исправить: GameException и наследники - проверяемые

--- src/ru/yandex/practicum/exceptions/gameExceptions/GameException.java
+++ src/ru/yandex/practicum/exceptions/gameExceptions/GameException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.exceptions.gameExceptions;
 
-public class GameException extends RuntimeException {
+public class GameException extends Exception {
 
     public GameException(String message) {
         super(message);
